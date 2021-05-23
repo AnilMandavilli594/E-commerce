ba02e9105e7bb55a3dc8d03da38a597fa4a7aee5
Anil Mandavilli <mandavillianil@gmail.com>
2021-05-23 21:33:21 +0530

    remove module

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'best quality',
+        },
+        {
+            _id:'2',
+            name:'Nike Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:130,
+            countInStock:20,
+            brand:'Nike',
+            rating:5.0,
+            numReviews:10,
+            description:'best quality'
+        },
+        {
+            _id:'3',
+            name:'Adidas Slim Shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:100,
+            countInStock:0,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'best quality'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim pant',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:90,
+            countInStock:1,
+            brand:'Nike',
+            rating:4,
+            numReviews:10,
+            description:'best quality'
+        },
+        {
+            _id:'5',
+            name:'Nike pant',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:80,
+            countInStock:14,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'best quality'
+        },
+        {
+            _id:'6',
+            name:'Adidas slim Pant',
+            category:'Shirts',
+            image:'/images/p6.jpg',
+            price:12,
+            countInStock:25,
+            brand:'Adidas',
+            rating:2,
+            numReviews:10,
+            description:'best quality'
+        },
+    ]
+}
+
+export default data;
